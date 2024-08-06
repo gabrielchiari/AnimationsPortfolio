7818429d18372eb2af560661bd65bc7a94fb6fb8
Gabriel Chiari <gabrielchiari.taf@gmail.com>
2024-08-06 14:00:08 -0500

    delete integration with cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import lenis from "astro-lenis";
 
-// import cloudflare from "@astrojs/cloudflare";
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), lenis()]
-  // output: "server",
-  // adapter: cloudflare()
+  integrations: [react(), tailwind(), lenis()],
+  site: 'https://AnimationsPortfolio.github.io',
 });
